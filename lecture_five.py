8663e9169ee0cf34784453200fccc920e0504072
PocoSelf <heykeshawself@gmail.com>
2026-05-20 00:27:21 +0530

    Started Loop

--- lecture_five.py
+++ lecture_five.py
@@ -1,1 +1,20 @@
-print("Hey There")
+# count = 1
+
+# while count <= 5 :
+#     print("hello")
+#     count += 1
+
+# i = 1
+# while i <= 5 :
+#     print("hello", i)
+#     i += 1
+
+i = 1
+while i <= 10 :
+    print(i)
+    i += 1
+
+print("Loop ended")
+    
+
+
