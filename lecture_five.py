88bd90281828e0fb54610ff5bf56182dc25efa4c
PocoSelf <heykeshawself@gmail.com>
2026-05-20 12:17:07 +0530

    Ques1

--- lecture_five.py
+++ lecture_five.py
@@ -9,12 +9,25 @@
 #     print("hello", i)
 #     i += 1
 
-i = 1
-while i <= 10 :
-    print(i)
-    i += 1
+# i = 1
+# while i <= 10 :
+#     print(i)
+#     i += 1
 
-print("Loop ended")
+# print("Loop ended")
+
+# i = 5
+# while i >= 1:
+#     print("hello", i)
+#     i -= 1
+# print("Loop Ended")
+
+a = 1
+while a <=100 :
+    print(a)
+    a += 1
+
+
     
 
 
