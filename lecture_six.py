2d74a1b6bd677cf67992be0bcfd495db8d671e7e
PocoSelf <heykeshawself@gmail.com>
2026-06-21 21:25:12 +0530

    Done L6

--- lecture_six.py
+++ lecture_six.py
@@ -106,6 +106,60 @@
 
 # odd_even(n)
 
+#Recursive Function
+# def show(n) :
+#     if (n == 0) :
+#         return
+#     print(n)
+#     show(n-1)
+#     print("END")
+# n = int(input("Enter a number : "))
+# show(n)
+
+# def fact(n) :
+#    if (n == 0 or n == 1) :
+#       return 1
+#    return fact(n-1) * n
+# print(fact(6))
+
+
+# def sum(n) :
+#     k = 0
+#     for i in range (1, n+1) :
+#        k += i
+#     print(k)
+
+# sum(3)
+
+# def calc_sum(n) :
+#     if (n == 0) :
+#         return 0
+#     return calc_sum(n-1)+n
+
+# sum = calc_sum(10)
+# print(sum)
+
+# marks = ["math", "phy", "chem", "bio"]
+# def marks_list(n) :
+#     for n in range(0, len(marks)) :
+#      print(marks[n])
+#     n += 1
+
+# marks_list(1)
+
+def print_list(list, idx=0) :
+    if (idx == len(list)) :
+        return
+    print(list[idx])
+    print_list(list, idx+1)
+fruits = ["mango", "litchi", "apple", "banana", "kiwi"]
+print_list(fruits)
+
+    
+
+
+
+
 
 
 
